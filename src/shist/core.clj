77109d04527596dcbc4c67584c196c93d97f6d8d
Matt Jones <jonesmr+github@gmail.com>
2012-06-12 03:09:23 +0000

    test handler

--- src/shist/core.clj
+++ src/shist/core.clj
@@ -1,18 +1,24 @@
 (ns shist.core
-  (:use compojure.core)
+  (:use compojure.core
+        [ring.middleware.params :only [wrap-params]]
+        [ring.middleware.keyword-params :only [wrap-keyword-params]])
   (:require [appengine-magic.core :as ae]))
 
 
-;(defn shist-app-handler [request]
-;  {:status 200
-;   :headers {"Content-Type" "text/plain"}
-;   :body "Hello, world!"})
-
-(defroutes shist-app-handler
+(defroutes shist-app-routes
   (GET "/" req
        {:status 200
         :headers {"Content-Type" "text/plain"}
-        :body "Hello, world!"})
+        :body "Hello, world! (updated)"})
+  (GET "/test/:id" [id & params]
+       (str "The ID is: " id " and param is: " (params :param)))
   )
+
+; Makes GET parameters work in dev-appserver
+; https://github.com/gcv/appengine-magic/issues/28
+(def shist-app-handler
+  (-> #'shist-app-routes
+      wrap-keyword-params
+      wrap-params))
 
 (ae/def-appengine-app shist-app #'shist-app-handler)
