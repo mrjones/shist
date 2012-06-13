c1cbdb2298d30ad082cffe2d77c5173afa17055d
Matt Jones <jonesmr+github@gmail.com>
2012-06-13 00:01:06 +0000

    saving and retrieving commands by hash

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shist "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.1.0"]]
   :dev-dependencies [[appengine-magic "0.5.0"]
                      [swank-clojure "1.3.0-SNAPSHOT"]])
--- src/shist/core.clj
+++ src/shist/core.clj
@@ -1,4 +1,5 @@
 (ns shist.core
+  (:import [java.security MessageDigest])
   (:use compojure.core
         [ring.middleware.params :only [wrap-params]]
         [ring.middleware.keyword-params :only [wrap-keyword-params]])
@@ -7,17 +8,48 @@
 
 (ds/defentity KeyValuePair [^:key key, value])
 
+(ds/defentity Command [ ^:key id, command, hostname, timestamp ])
+
+(defn md5
+  "Generate a md5 checksum for the given string"
+  [token]
+  (let [hash-bytes
+        (doto (java.security.MessageDigest/getInstance "MD5")
+          (.reset)
+          (.update (.getBytes token)))]
+    (.toString
+     (new java.math.BigInteger 1 (.digest hash-bytes)) ; Positive and the size of the number
+              16))) ; Use base16 i.e. hex
+
 (defroutes shist-app-routes
   (GET "/" req
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body "Hello, world! (updated 4)"})
+  ;; Insert a new command into the archive
+  ;; Make this POST-only
+  (ANY "/commands/" [& params]
+       (let [cmd (Command. (md5 (str (:host params) (:ts params)))
+                           (:cmd params)
+                           (:host params)
+                           (:ts params))]
+         (str "Timestamp: " (:timestamp cmd)
+              " host: " (:hostname cmd)
+              " cmd: " (:command cmd)
+              " id: " (:id cmd))
+;;         (ds/save! cmd)
+         ))
+  (GET "/command/:cmdid" [cmdid]
+       (let [cmd (ds/retrieve Command cmdid)]
+         (if (nil? cmd)
+           {:status 404 :body (str cmdid " not found")}
+           {:status 200 :body (:command cmd)})))
   (GET "/store/:key/:value" [key value]
        ; Do a lookup (check for dupes) first
        (let [kv (KeyValuePair. "foo" value)]
          (ds/save! kv)
          (str "Setting " key " to " value ". P.S. " (:key kv) (:value kv))))
-  (GET "/lookup/:key" [key] #".*"
+  (GET "/lookup/:key" [key]
        ; Figure out how to construct a key to make ds/retrieve work
        (let [kv (first (ds/query :kind KeyValuePair :filter (= :key key)))]
          (if (nil? kv)
@@ -25,6 +57,7 @@
            (str "Looking up " key ". Got " (:value kv)))))
   (GET "/favicon.ico" [] { :status 404 })
   )
+
 
 ; Makes GET parameters work in dev-appserver
 ; https://github.com/gcv/appengine-magic/issues/28
