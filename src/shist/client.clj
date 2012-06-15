f69f57f5bea4d90e96b55e6612a169b53144c59a
Matt Jones <jonesmr+github@gmail.com>
2012-06-15 16:41:34 +0000

    client can do inserts too

--- src/shist/client.clj
+++ src/shist/client.clj
@@ -1,6 +1,23 @@
 (ns shist.client
   (:require [clj-http.client :as http]))
 
-(defn hitroot []
-  (http/get "http://localhost:8081"))
-  
+(defrecord Target [hostname port])
+
+(def localserver (Target. "localhost" 8081))
+
+(defn serverstr [target path]
+  (str "http://" (:hostname target) ":" (:port target) path))
+
+(defn listcommands [target]
+  (http/get (serverstr target "/commands/")))
+
+(defn getcommand [target md5]
+  (http/get (serverstr target (str "/command/" md5))))
+
+(defn addcommand [target cmdhost cmdts cmdbody]
+  (http/post (serverstr target "/commands/")
+             {:body (http/generate-query-string
+                     (hash-map :cmd cmdbody :host cmdhost :ts cmdts))
+              :content-type "application/x-www-form-urlencoded"}))
+
+
